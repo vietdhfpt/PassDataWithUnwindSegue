e3da30ed8ec335783be5b242720a226585318dfe
Đỗ Việt <dohoangviet.fpt@gmail.com>
2017-04-26 09:42:16 +0700

    init commit

---
+++ PassDataWithUnwindSegue/PassDataWithUnwindSegueTests/PassDataWithUnwindSegueTests.swift
@@ -0,0 +1,36 @@
+//
+//  PassDataWithUnwindSegueTests.swift
+//  PassDataWithUnwindSegueTests
+//
+//  Created by Đỗ Việt on 4/26/17.
+//  Copyright © 2017 Do Viet. All rights reserved.
+//
+
+import XCTest
+@testable import PassDataWithUnwindSegue
+
+class PassDataWithUnwindSegueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
